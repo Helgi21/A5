426c9065024c280b9039def1286af6842fe0a21c
Helgi Steinarr Juliusson <helgi21@ru.is>
2021-09-02 14:00:20 +0000

    Completed

--- max_int.py
+++ max_int.py
@@ -1,6 +1,9 @@
 # Mun loopa biðjandi um input þangað til notandinn slær inn mínus tölu og ber innsláða tölu við fyrverandi max
-
-
-num_int = int(input("Input a number: "))    # Do not change this line
-# Fill in the missing code
+num_int = 0
+max_int = 0
+while num_int >= 0:
+    num_int = int(input("Input a number: "))    # Do not change this line
+    if (num_int > max_int):
+        max_int = num_int
+    
 print("The maximum is", max_int)    # Do not change this line
