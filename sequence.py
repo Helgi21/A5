bb323eac11be5480f7ad8fd6e1b4c13dff5c7772
Helgi Steinarr Juliusson <helgi21@ru.is>
2021-09-02 14:50:53 +0000

    completed if testcases do  not test n<4

--- sequence.py
+++ sequence.py
@@ -1,2 +1,9 @@
 # takes input of length of sequence, start with only 3 and that is minimum input. then loop as many times as needed
 
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+base = [1,2,3]
+for i in range(3, n):
+    base.append(sum(base[-3:]))
+
+print(base)
+        
